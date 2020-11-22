36ad11ea46a3c64eb14ae2b0f72ad419642ba742
ElianMartinez <Elianmartinez157@gmail.com>
2020-11-22 00:46:37 -0400

    header casi completo, falta cositas y colocar los links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import react  from 'react';
 import './App.css';
+import Header from './components/header';
 import Nav from './components/nav';
 
 
 class App extends react.Component {
   render() {
-    return <div>
-      <Nav></Nav>
-    </div>
+    return <>
+     
+    <Header />
+    
+    </>
   }
 }
 
